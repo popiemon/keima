bbb166237e361cf0a5e1a722015d07cd1138cc1e
popiemon <kryo.over@gmail.com>
2025-11-26 00:49:56 +0900

    testをチケットの購入と支払いに絞る

--- tests/app/test_buyticket_reward.py
+++ tests/app/test_buyticket_reward.py
@@ -10,15 +10,15 @@ def client():
 
 
 @pytest.mark.parametrize(
-    ("team_name", "game_id", "tickets", "result", "expected_reward"),
+    ("team_name", "game_id", "tickets", "coins", "expected_coins"),
     [
-        ("A", 1, [{"ticket_type": "win", "one": 1, "unit": 2}], [1, 2, 3, 4], 8),
+        ("A", 1, [{"ticket_type": "win", "one": 1, "unit": 2}], 100, 98),
         (
             "B",
             1,
             [{"ticket_type": "exacta", "one": 1, "two": 2, "unit": 3}],
-            [1, 2, 3, 4],
-            36,
+            100,
+            97,
         ),
         (
             "C",
@@ -27,19 +27,25 @@ def client():
                 {"ticket_type": "trifecta", "one": 1, "two": 2, "three": 3, "unit": 1},
                 {"ticket_type": "win", "one": 4, "unit": 5},
             ],
-            [1, 2, 3, 4],
-            28,
+            100,
+            94,
         ),
     ],
 )
-def test_buyticket_reward(
+def test_buyticket(
     client: TestClient,
     team_name: str,
     game_id: int,
     tickets: list[dict],
-    result: list[int],
-    expected_reward: int,
+    coins: int,
+    expected_coins: int,
 ) -> None:
+    # Set coins
+    _ = client.post(
+        "/admin/set_coins",
+        json={"team_name": team_name, "coins": coins, "game_id": game_id},
+    )
+
     # Set initial coins and allow ticket purchasing
     client.post("/admin/set_coins", json={"team_name": team_name, "coins": 1000})
     _ = client.post(
@@ -64,10 +70,10 @@ def test_buyticket_reward(
     }
 
     # Close ticket purchasing
-    # client.post(
-    #     "/admin/set_race_state",
-    #     json={"race_id": game_id, "ticket_buy": False},
-    # )
+    client.post(
+        "/admin/set_race_state",
+        json={"race_id": game_id, "ticket_buy": False},
+    )
 
     # # Save race result
     # client.post("/admin/save_race_result", json=result)
