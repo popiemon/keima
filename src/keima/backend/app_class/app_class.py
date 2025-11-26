97bf62b63f4848269b92fe8e9d0341ca80ba90e8
popiemon <kryo.over@gmail.com>
2025-11-27 00:11:11 +0900

    race state

--- src/keima/backend/app_class/app_class.py
+++ src/keima/backend/app_class/app_class.py
@@ -34,16 +34,32 @@ class BuyTicketRequest(BaseModel):
 class RaceState(BaseModel):
     """raceの状態を保存するクラス
 
+    gameの進行の仕方は、
+    game_id=0, ticket_buy=False, ticket_paid=False
+        ↓ 最初の状態
+    game_id=0, ticket_buy=True, ticket_paid=False
+        ↓ チケット購入可能状態
+    game_id=0, ticket_buy=False, ticket_paid=False
+        ↓ チケット購入締め切り状態
+    game_id=0, ticket_buy=False, ticket_paid=True
+        ↓ チケットの支払いが完了状態
+    game_id=1, ticket_buy=False, ticket_paid=False
+        ↓ レース結果に基づいて配当金が支払われ、次のレースへ
+
     Parameters
     ----------
     game_id : int
         レース番号
     ticket_buy : bool
         チケット購入可能かどうか
+    ticket_paid : bool
+        チケットの支払いが完了しているかどうか。Trueの場合、チケット購入はできない。
+        この後、レース結果に基づいて配当金が支払われ、 ticket_paid は False にリセットされる。
     """
 
     game_id: int = 0
     ticket_buy: bool = False
+    ticket_paid: bool = False
 
 
 class SetCoinsRequest(BaseModel):
